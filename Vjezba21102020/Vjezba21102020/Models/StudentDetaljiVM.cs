c966f4519726af1f67e2882af49845ec2b2597dc
Zorka Kunovac <61622729+ZorkaKunovac@users.noreply.github.com>
2020-11-25 18:49:02 +0100

    Dodan view Detalji i StudentDetaljiVM

---
+++ Vjezba21102020/Vjezba21102020/Models/StudentDetaljiVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vjezba21102020.Models
+{
+    public class StudentDetaljiVM
+    {
+        public string NazivPredmeta { get; set; }
+        public int BrojcanaOcjena { get; set; }
+        public DateTime Datum { get; set; }
+    }
+}
