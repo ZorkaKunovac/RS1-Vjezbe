5d9fa310ed9c781cfcccca60d59df6cb6ff4b5d1
Zorka Kunovac <61622729+ZorkaKunovac@users.noreply.github.com>
2020-11-26 21:05:06 +0100

    Dodana klasa PrisustvoNaNastavi

---
+++ Vjezba21102020/Vjezba21102020/Controllers/TestniPodaciController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vjezba21102020.Controllers
+{
+    public class TestniPodaciController : Controller
+    {
+        public IActionResult Index()
+        {
+           // DbInicijalizator.Generisi();
+            return View();
+        }
+    }
+}
---
+++ Vjezba21102020/podaci/EntityModels/PrisustvoNaNastavi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vjezba21102020.EntityModels;
+
+namespace podaci.EntityModels
+{
+    class PrisustvoNaNastavi
+    {
+        public int StudentID { get; set; }
+        public Student Student { get; set; }
+        public int ID { get; set; }
+        public int PredmetID { get; set; }
+        public Predmet Predmet { get; set; }
+        public DateTime Datum { get; set; }
+    }
+}
