02f4851ce24fb036e97467b042e4f11f954ecfb7
Zorka Kunovac <61622729+ZorkaKunovac@users.noreply.github.com>
2020-12-28 23:27:20 +0100

    Dodan AutentifikacijaIndexVM i Index View

--- Vjezba21102020/Vjezba21102020/Controllers/AutentifikacijaController.cs
+++ Vjezba21102020/Vjezba21102020/Controllers/AutentifikacijaController.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using podaci.EntityModels;
+using Vjezba21102020.EF;
+using Vjezba21102020.Models;
 
 namespace Vjezba21102020.Controllers
 {
     public class AutentifikacijaController : Controller
     {
+        MojDbContext db = new MojDbContext();
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Login(AutentifikacijaIndexVM x)
+        {
+            KorisnickiNalog nalog = db.KorisnickiNalog.Where(k => k.KorisnickoIme == x.username && k.Lozinka == x.password).SingleOrDefault();
+            if (nalog == null)
+            {
+                TempData["porukaGreska"] = "Neispravan username/password";
+                return View("Index");
+            }
+            return Redirect("/");
         }
     }
 }
---
+++ Vjezba21102020/Vjezba21102020/Models/AutentifikacijaIndexVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vjezba21102020.Models
+{
+    public class AutentifikacijaIndexVM
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
