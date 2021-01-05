1db3271472daded13543e7104dafc631a42d31fd
Zorka Kunovac <61622729+ZorkaKunovac@users.noreply.github.com>
2021-01-05 17:51:33 +0100

    Implementirana funkcija DodajNastavnika u Home

--- Autentifikacija/Autentifikacija/Controllers/HomeController.cs
+++ Autentifikacija/Autentifikacija/Controllers/HomeController.cs
@@ -6,16 +6,22 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Autentifikacija.Models;
+using Data.EntityModels;
+using Microsoft.AspNetCore.Identity;
+using Autentifikacija.Data;
 
 namespace Autentifikacija.Controllers
 {
     public class HomeController : Controller
     {
+        private UserManager<Korisnik> _userManager;
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private ApplicationDbContext _db;
+        public HomeController(ILogger<HomeController> logger, UserManager<Korisnik> userManager, ApplicationDbContext db)
         {
             _logger = logger;
+            _userManager = userManager;
+            _db = db;
         }
 
         public IActionResult Index()
@@ -33,5 +39,35 @@ namespace Autentifikacija.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public string DodajNastavnika(string ime, string prezime)
+        {
+            string email = ime + "." + prezime + "@edu.fit.ba";
+            var korisnik = new Korisnik
+            {
+                Email = email,
+                UserName = email,
+                Ime = ime,
+                Prezime = prezime,
+                EmailConfirmed = true,
+            };
+            IdentityResult result = _userManager.CreateAsync(korisnik, "Mostar2020!").Result;
+
+            if (!result.Succeeded)
+            {
+                return "errors: " + string.Join('|', result.Errors);
+            }
+
+            Nastavnik nastavnik = new Nastavnik
+            {
+                Korisnik = korisnik,
+                Zvanje = "prof dr"
+            };
+            _db.Nastavnik.Add(nastavnik);
+            _db.SaveChanges();
+            return "Nastavnik je uspješno dodat";
+        }
+
+
     }
 }
