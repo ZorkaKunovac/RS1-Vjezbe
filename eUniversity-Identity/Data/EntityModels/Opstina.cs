5d0c93ca022a57073d00d584ad5c8b0039c97c72
Zorka Kunovac <61622729+ZorkaKunovac@users.noreply.github.com>
2021-01-08 22:25:35 +0100

    Kreiran novi projekat eUniversity+Identity

---
+++ eUniversity-Identity/Data/EntityModels/Opstina.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vjezba21102020.EntityModels
+{
+    public class Opstina
+    {
+        public int ID { get; set; }
+        public string Naziv { get; set; }
+    }
+}
